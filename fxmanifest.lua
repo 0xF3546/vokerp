6f24e14fe548f42e7b7955a6364723a6a37aace2
0xF3546 <99101632+0xF3546@users.noreply.github.com>
2025-02-19 17:22:32 +0100

    updated fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,3 +6,10 @@ game 'gta5'
 server_script 'dist/server/**/*.js'
 client_script 'dist/client/**/*.js'
 
+ui_page 'web/build/index.html'
+
+files {
+	'web/build/index.html',
+	'web/build/**/*',
+    'node_modules/**/*',
+}
