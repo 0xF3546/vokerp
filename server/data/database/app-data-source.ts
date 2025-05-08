e609ef6b134700d6303d6a4ed99341cc313b4213
0xF3546 <99101632+0xF3546@users.noreply.github.com>
2025-05-08 09:38:02 +0200

    change pwd

--- server/data/database/app-data-source.ts
+++ server/data/database/app-data-source.ts
@@ -43,7 +43,7 @@ export const dataSource = new DataSource({
     host: "45.13.227.193",
     port: 3306,
     username: "gta_gameserver",
-    password: "1V0qB90X£VK+",
+    password: "1V0qB90XVK+",
     database: "gta",
     logging: false,
     synchronize: true,
